1021b1627e495cc414ad5f68a2f7a21435859ba5
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2023-05-12 18:13:46 +0800

    Code minification

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cictwvsu.com/',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind(), react(), compress()]
 });
