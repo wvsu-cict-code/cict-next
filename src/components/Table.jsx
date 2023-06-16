86975bd73b8dd7d9b7f910203d76e9b72e45ea1f
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2023-06-16 18:36:58 +0800

    Updated with results

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,55 +5,115 @@ import {
   flexRender,
   getCoreRowModel,
   useReactTable,
-} from '@tanstack/react-table';
+} from "@tanstack/react-table";
 
-const columnHelper = createColumnHelper()
+const columnHelper = createColumnHelper();
 
-const columns = [
-  columnHelper.accessor('name', {
-    header: ()=> 'Qualified Applicant',
-    cell: info => info.getValue(),
-    footer: info => info.column.id,
+const columns_qualified = [
+  columnHelper.accessor("fullname", {
+    header: (indicator) => `${indicator.toUpperCase()} Qualified Applicant`,
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
   }),
-  columnHelper.accessor('time', {
-    header: () => 'Time',
-    cell: info => info.renderValue(),
-    footer: info => info.column.id,
-  }),
-  columnHelper.accessor('room', {
-    header: () => 'Room Assignment',
-    cell: info => info.renderValue(),
-    footer: info => info.column.id,
-  }),
-  columnHelper.accessor('day', {
-    header: () => 'Date',
-    cell: info => info.renderValue(),
-    footer: info => info.column.id,
-  }),
-]
+];
 
-export default function App({dataset}) {
-  const [data, setData] = React.useState(() => [...dataset])
-  const rerender = React.useReducer(() => ({}), {})[1]
+const columns_waitlisted = [
+  columnHelper.accessor("priority", {
+    header: (indicator) => "Priority #",
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("fullname", {
+    header: (indicator) => `${indicator.toUpperCase()} Waitlisted Applicant`,
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+];
 
-  const table = useReactTable({
+const columns_recommended = [
+  columnHelper.accessor("fullname", {
+    header: (indicator) => `${indicator.toUpperCase()} Recommended Applicant`,
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+];
+
+export default function App({ dataset, course, status }) {
+  const [data, setData] = React.useState(() => [...dataset]);
+  const rerender = React.useReducer(() => ({}), {})[1];
+  var course_name = "";
+  var table = useReactTable({
     data,
-    columns,
+    columns: {},
     getCoreRowModel: getCoreRowModel(),
-  })
+  });
+
+  switch (status) {
+    case "qualified":
+      table = useReactTable({
+        data,
+        columns: columns_qualified,
+        getCoreRowModel: getCoreRowModel(),
+      });
+      break;
+    case "waitlisted":
+      table = useReactTable({
+        data,
+        columns: columns_waitlisted,
+        getCoreRowModel: getCoreRowModel(),
+      });
+      break;
+    case "recommended":
+      table = useReactTable({
+        data,
+        columns: columns_recommended,
+        getCoreRowModel: getCoreRowModel(),
+      });
+      break;
+    default:
+      table = useReactTable({
+        data,
+        columns: {},
+        getCoreRowModel: getCoreRowModel(),
+      });
+      break;
+  }
+
+  switch (course) {
+    case "bsit":
+      course_name = "Bachelor of Science in Information Technology";
+      break;
+    case "bscs":
+      course_name = "Bachelor of Science in Computer Science";
+      break;
+    case "bsis":
+      course_name = "Bachelor of Science in Information Systems";
+      break;
+    case "bsemc":
+      course_name =
+        "Bachelor of Science in Entertainment and Multimedia Computing";
+      break;
+    case "blis":
+      course_name = "Bachelor of Library and Information Science";
+      break;
+    default:
+      var course_name = "";
+      break;
+  }
 
   return (
-    <div className="p-2 text-sm">
+    <div className="p-2 text-sm max-w-md mx-auto">
+      {/* <h2 className="text-lg font-bold mb-2">{course_name}</h2> */}
       <table>
         <thead>
-          {table.getHeaderGroups().map(headerGroup => (
+          {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
-              {headerGroup.headers.map(header => (
+              {headerGroup.headers.map((header) => (
                 <th className="p-2 border text-left" key={header.id}>
                   {header.isPlaceholder
                     ? null
                     : flexRender(
-                        header.column.columnDef.header,
+                        header.column.columnDef.header(course),
                         header.getContext()
                       )}
                 </th>
@@ -62,9 +122,9 @@ export default function App({dataset}) {
           ))}
         </thead>
         <tbody>
-          {table.getRowModel().rows.map(row => (
+          {table.getRowModel().rows.map((row) => (
             <tr className="border" key={row.id}>
-              {row.getVisibleCells().map(cell => (
+              {row.getVisibleCells().map((cell) => (
                 <td className="border p-2" key={cell.id}>
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </td>
@@ -74,5 +134,5 @@ export default function App({dataset}) {
         </tbody>
       </table>
     </div>
-  )
+  );
 }
