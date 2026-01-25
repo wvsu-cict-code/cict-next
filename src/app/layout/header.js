09b82a3eb7cbb77ec8f95f8d95a0700a056abaf6
John Manuel Carado <carado.johnp11q@gmail.com>
2026-01-25 23:53:54 +0800

    feat: Add Tailwind class sorter from prettier

--- src/app/layout/header.js
+++ src/app/layout/header.js
@@ -2,22 +2,28 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="flex flex-row gap-x-3 text-lg">
-      <Link href="/" className="hover:underline">
+      <Link href="/" className="hover:text-blue-800 hover:underline">
         Home
       </Link>
-      <Link href="/about" className="hover:underline">
+      <Link href="/about" className="hover:text-blue-800 hover:underline">
         About
       </Link>
-      <Link href="/programs" className="hover:underline">
+      <Link href="/programs" className="hover:text-blue-800 hover:underline">
         Programs
       </Link>
-      <Link href="/faculty-profiles" className="hover:underline">
+      <Link
+        href="/faculty-profiles"
+        className="hover:text-blue-800 hover:underline"
+      >
         Faculty
       </Link>
-      <Link href="/contact" className="hover:underline">
+      <Link href="/contact" className="hover:text-blue-800 hover:underline">
         Contact
       </Link>
-      <Link href="/news-announcements" className="hover:underline">
+      <Link
+        href="/news-announcements"
+        className="hover:text-blue-800 hover:underline"
+      >
         News
       </Link>
     </header>
