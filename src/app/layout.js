aeb45c2b6625ff36b95e84a470de711e10852735
John Manuel Carado <carado.johnp11q@gmail.com>
2026-01-25 23:41:08 +0800

    feat: Create temporary header for easier nav links

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Oswald } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const oswald = Oswald({
   variable: "--font-oswald",
@@ -9,7 +10,30 @@ const oswald = Oswald({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${oswald.variable}`}>{children}</body>
+      <body className={`${oswald.variable}`}>
+        {/* Temporary header */}
+        <header className="flex flex-row gap-x-3 text-lg">
+          <Link href="/" className="hover:underline">
+            Home
+          </Link>
+          <Link href="/about" className="hover:underline">
+            About
+          </Link>
+          <Link href="/programs" className="hover:underline">
+            Programs
+          </Link>
+          <Link href="/faculty-profiles" className="hover:underline">
+            Faculty
+          </Link>
+          <Link href="/contact" className="hover:underline">
+            Contact
+          </Link>
+          <Link href="/news-announcements" className="hover:underline">
+            News
+          </Link>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
