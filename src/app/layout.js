0b6e15244172d5067f43f168bd3a498bb4292119
John Manuel Carado <carado.johnp11q@gmail.com>
2026-01-25 22:58:29 +0800

    feat: Add Oswald typeface

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Oswald } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const oswald = Oswald({
+  variable: "--font-oswald",
   subsets: ["latin"],
 });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body className={`${oswald.variable}`}>{children}</body>
     </html>
   );
 }
