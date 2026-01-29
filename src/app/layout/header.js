831c33a2baf570d71e2525449e900d2192c9fde9
joshuavillamora <earikjoshua@gmail.com>
2026-01-30 04:57:45 +0800

    feat: Add logo and wordmark for desktop header

--- src/app/layout/header.js
+++ src/app/layout/header.js
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
 
 const active =
@@ -11,25 +12,32 @@ export default function Header({ inter }) {
   const pathname = usePathname();
 
   return (
-    <header className={`${inter.className} flex flex-row gap-x-12 text-lg justify-center px-6 py-6 font-medium`}>
-      <Link href="/programs" className={(pathname === "/programs") ? active : inactive}>
-        PROGRAMS
+    <header className={`${inter.className} flex flex-row text-lg px-6 py-6 font-medium`}>
+      <Link href="/" className="flex items-center gap-x-2 ml-45">
+        <Image src="/cict-emblem-dark.png" width={37} height={38} />
+        <Image src="/cict-wordmark.png" width={112} height={25} />
       </Link>
-      <Link
-        href="/news-announcements"
-        className={(pathname === "/news-announcements") ? active : inactive}
-      >
-        NEWS
-      </Link>
-      <Link
-        href="/faculty-profiles"
-        className={(pathname === "/faculty-profiles") ? active : inactive}
-      >
-        FACULTY
-      </Link>
-      <Link href="/contact" className={(pathname === "/contact") ? active : inactive}>
-        CONTACT
-      </Link>
+    
+      <div className="absolute left-1/2 -translate-x-1/2 flex gap-x-12 text-lg mt-1"> 
+        <Link href="/programs" className={(pathname === "/programs") ? active : inactive}>
+          PROGRAMS
+        </Link>
+        <Link
+          href="/news-announcements"
+          className={(pathname === "/news-announcements") ? active : inactive}
+        >
+          NEWS
+        </Link>
+        <Link
+          href="/faculty-profiles"
+          className={(pathname === "/faculty-profiles") ? active : inactive}
+        >
+          FACULTY
+        </Link>
+        <Link href="/contact" className={(pathname === "/contact") ? active : inactive}>
+          CONTACT
+        </Link>
+      </div>
     </header>
   );
 }
