1bb2e90c4476ceeada50b7e3e01f527f9af91fd0
John Manuel Carado <carado.johnp11q@gmail.com>
2026-02-02 21:49:00 +0800

    styles(font): Remove next google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
-import { Oswald } from "next/font/google";
 import "./globals.css";
 import Header from "./layout/header";
-
-const oswald = Oswald({
-  variable: "--font-oswald",
-  subsets: ["latin"],
-});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${oswald.variable}`}>
+      <body>
         <Header />
         {children}
       </body>
