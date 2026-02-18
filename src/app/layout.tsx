6fd775ff4a674e2972f5960f79e10fb3b951343b
Wracell <rhazeljay.gumacal@wvsu.edu.ph>
2026-02-18 15:24:23 +0800

    feat: Add footer in layout and styles for footer to always stay in bottom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import "./globals.css";
 import Header from "./layout/header";
-
+import Footer from "./layout/footer";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+     <body style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
         <Header />
-        {children}
+      
+        <main style={{ flex: 1 }}>
+          {children}
+        </main>
+
+        <Footer />
       </body>
     </html>
   );
