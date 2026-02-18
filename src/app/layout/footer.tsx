eefeaf5e99631db37e25374289a0d75e85ceaeb7
Wracell <rhazeljay.gumacal@wvsu.edu.ph>
2026-02-18 19:50:47 +0800

    feat: Add left section of footer

--- src/app/layout/footer.tsx
+++ src/app/layout/footer.tsx
@@ -1,7 +1,56 @@
 export default function Footer() {
   return (
-    <footer>
-  
+    <footer className="bg-[#1e1e1e] relative overflow-hidden">
+    
+      <div className="w-auto mx-auto px-8 py-12 relative z-10">
+        <div className="grid grid-cols-[2fr_1fr] justify-between">
+          
+          {/* Left Column: Branding and Contact */}
+          <div>
+            {/* Logo Section */}
+            <div className="flex items-center mb-2 ml-32">
+              {/* Logo SVG Placeholder */}
+             <img
+              src="/footer_assets/cict-small-icon.svg" 
+              className="w-18 h-18"
+              />
+            
+              <h2 className="text-5xl font-medium uppercase leading-4 flex gap-3 text-white">
+                WVSU <span className="font-light font-major">CICT</span> 
+              </h2>
+            </div>
+
+            {/* Description */}
+            <p className="text-base text-left font-minor text-white max-w-170 ml-35 mb-18 leading-6">
+              The official website of the <span className="text-orange-light font-minor">WVSU College of Information and Communications Technology. </span><br /> We aim to foster a resilient, innovative, and digitally-ready Western Visayas - producing competent, globally competitive, and technologically skilled harbingers of change.
+            </p>
+
+            {/* Social Media Section */}
+            <div>
+              <h3 className="text-2xl font-normal mb-4 ml-35 text-white tracking-wide">
+                Get in touch with <span className="text-orange-light font-major">us</span>
+              </h3>
+              <div className="flex ml-33">
+                {/* External links: Standard <a> tags are best practice here */}
+                <a href="https://web.facebook.com/cictwvsu" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white transition-colors duration-200">
+              <img
+              src="/footer_assets/footer-fb.svg" 
+              className="w-7 h-5"
+              />
+                </a>
+               
+                <a href="mailto:cict@wvsu.edu.ph" className="text-gray-300 hover:text-white transition-colors duration-200">
+              <img
+              src="/footer_assets/footer-mail.svg" 
+              className="w-10 h-6 brightness-0 invert"
+              />
+                </a>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
     </footer>
   );
 }
