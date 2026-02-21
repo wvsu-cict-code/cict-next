47d14f768f8977502f4b5dc0069b8cefe15cdedf
Wracell <rhazeljay.gumacal@wvsu.edu.ph>
2026-02-21 17:33:01 +0800

    fix: remove the modifications done in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,11 @@ import Footer from "./layout/footer";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-     <body style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+     <body >
         <Header />
-      
-        <main style={{ flex: 1 }}>
+        <main>
           {children}
         </main>
-
         <Footer />
       </body>
     </html>
