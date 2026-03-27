93bba5db1c05d3e9f35e270b8cb4350e4c4eec59
joshuavillamora <earikjoshua@gmail.com>
2026-03-27 12:22:36 +0800

    style: Add landing page CTA styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 export default function Home() {
+
+  const button_big = "inline-block border-8 rounded-2xl w-xs h-10";
+
   return (
     <main>
       {/*Landing Page*/}
@@ -10,9 +13,9 @@ export default function Home() {
         </div>
 
         {/*Buttons*/}
-        <div>
-          <a href="#courses-offered">COURSES OFFERED</a>
-          <a href="#why-choose-us">WHY CHOOSE US</a>
+        <div className="flex justify-center items-center mt-16 gap-20 text-center text-white text-base font-bold">
+          <a href="#courses-offered" className={` ${button_big} border-[var(--color-orange-light)] bg-[var(--color-orange-light)]`}>COURSES OFFERED</a>
+          <a href="#why-choose-us" className={` ${button_big} border-[#373737] bg-[#373737]`}>WHY CHOOSE US</a>
         </div>
 
         {/*Tagline*/}
