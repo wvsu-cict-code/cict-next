52f4e7a7c7f4e11f53b51edd4b5203a23d2f3515
joshuavillamora <earikjoshua@gmail.com>
2026-03-27 10:38:59 +0800

    feat: Add landing page skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello from homepage!</h1>
+      {/*Landing Page*/}
+      <section>
+        {/*Header*/}
+        <div>
+          <h1>Building the Digital Leaders of Tomorrow</h1>
+          <h2>COLLEGE OF INFORMATION AND COMMUNICATIONS TECHNOLOGY</h2>
+        </div>
+
+        {/*Buttons*/}
+        <div>
+          <a href="#courses-offered">COURSES OFFERED</a>
+          <a href="#why-choose-us">WHY CHOOSE US</a>
+        </div>
+
+        {/*Tagline*/}
+        <p>We have been shaping careers for technological trailblazers across industries for 23 years.</p>
+      </section>
     </main>
   );
 }
